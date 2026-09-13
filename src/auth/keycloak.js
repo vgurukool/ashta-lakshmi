b04050f01c72e9be4ade1fa858e4369ab5a009db
Ayush Singhaniya <ayush.o.singhaniya@gmail.com>
2026-09-12 20:51:21 -0500

    feat(auth): point Keycloak SSO to platform.vgurukool.com and bump image to v1.2.0

--- src/auth/keycloak.js
+++ src/auth/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  url: 'https://vgurukool.com/keycloak',
+  url: 'https://platform.vgurukool.com/keycloak',
   realm: 'cnoe',
   clientId: 'vgurukool-apps'
 });
